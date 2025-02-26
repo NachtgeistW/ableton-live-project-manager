f8cb6e5802a70980d105017c5a686a815970dd39
NachtgeistW <nachtgeistw@outlook.com>
2025-02-26 22:01:54 +0800

    add app.axaml to let it run

---
+++ App.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core.Plugins;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace AbletonProjectManager
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            // Remove data validation to improve performance when not needed
+            BindingPlugins.DataValidators.RemoveAt(0);
+            
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow();
+            }
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -35,24 +35,4 @@ namespace AbletonProjectManager
                 .UsePlatformDetect()
                 .LogToTrace();
     }
-    
-    public class App : Application
-    {
-        public override void Initialize()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
-
-        public override void OnFrameworkInitializationCompleted()
-        {
-            // Remove data validation to improve performance when not needed
-            BindingPlugins.DataValidators.RemoveAt(0);
-            
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                desktop.MainWindow = new MainWindow();
-            }
-            base.OnFrameworkInitializationCompleted();
-        }
-    }
 }
